a133c3c8094fc2fe2e73d470aca519fb3209013b
John Ericson <j.r.ericson01@gmail.com>
2023-06-25 22:36:58 -0700

    NotFound Page Added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,13 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import HomePage from "./pages/HomePage"
+import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import { useAppDispatch, useAppSelector } from './app/hooks';
 import { getProfiles } from './features/profiles/profileSlice';
 import Profile from './components/Profile';
 import ProfilesPage from './pages/ProfilesPage';
-
+import "react-toastify/dist/ReactToastify.css";
+import NotFound from './components/NotFound';
 
 const App: FC = () => {
 
@@ -23,19 +24,16 @@ const App: FC = () => {
           <Header />
           <main className="py-3">
             <Routes>
-              <Route path="/" element={<HomePage />}></Route>
-              <Route path="/profile/all" element={<ProfilesPage />}></Route>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/profile/all" element={<ProfilesPage />} />
+              <Route path="/about" element={<AboutPage />} />
+              <Route path="*" element={<NotFound />} />
             </Routes>
-            <Routes>
-              <Route
-							path="/about"
-							element={<AboutPage />}
-						></Route>
-            </Routes>
+            <ToastContainer theme='dark' />
+
           </main>
           <Footer />
         </Router>
-        {/* <ToastContainer /> */}
       </>
 
     </div>
